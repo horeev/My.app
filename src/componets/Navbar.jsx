26c29b050ee81a5abe22bcc7e18ab2dd23c79f67
Sergei Petrov <horeev@outlook.com>
2020-09-26 21:04:10 +0300

    New file

---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar =()=> {
+    return < nav className='nav'>
+        <div >
+            <a>Profile</a>
+        </div>
+        <div>
+            <a>Messager</a>
+        </div>
+        <div>
+            <a>News</a>
+        </div>
+        <div>
+            <a>Music</a>
+        </div>
+        <div>
+            <a>Main content</a>
+        </div>
+    </nav>
+}
+export default Navbar
---
+++ src/componets/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div><img src="https://images.traum-ferienwohnungen.de/236784/8046961/46/villa-paradis-6-at-paradis-resort.jpg"/>
+        </div>
+        <div>
+            ava + discription
+        </div>
+        <div>
+            My post
+        </div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>
+            New Post
+        </div>
+    </div>;
+}
+export default Profile

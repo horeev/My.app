748c61c26ff10767c702f4ae0c1c8bbc8200ab65
Sergei Petrov <horeev@outlook.com>
2020-09-27 11:57:08 +0300

    Css modules

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import classes from "./Navbar.module.css";
 
 const Navbar =()=> {
-    return < nav className='nav'>
-        <div >
+    return < nav className={classes.nav}>
+        <div className={classes.item}>
             <a>Profile</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Messager</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>News</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Music</a>
         </div>
-        <div>
+        <div className={classes.item}>
             <a>Main content</a>
         </div>
     </nav>
--- src/componets/Profile.jsx
+++ src/componets/Profile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import b from "./Profile.module.css";
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={b.content}>
         <div><img src="https://images.traum-ferienwohnungen.de/236784/8046961/46/villa-paradis-6-at-paradis-resort.jpg"/>
         </div>
         <div>
@@ -9,12 +10,21 @@ const Profile = () => {
         </div>
         <div>
             My post
+            <div>
+                New Post
+            </div>
+            <div className={b.posts}>
+                <div className={b.item}>
+                    Post
+                </div>
+            </div>
+            <div className={b.item}>
+                Post2
+            </div>
+
         </div>
-        <div>Post 1</div>
-        <div>Post 2</div>
-        <div>
-            New Post
-        </div>
+
+
     </div>;
 }
 export default Profile
